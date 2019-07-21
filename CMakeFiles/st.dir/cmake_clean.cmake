570a00e12c8325cc16d2b7edfd55f865e651a690
TresSheep <edvin.carlsson@outlook.com>
2019-07-21 20:29:19 +0200

    first commit

---
+++ CMakeFiles/st.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/st.dir/src/main.cpp.o"
+  "bin/st.pdb"
+  "bin/st"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/st.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+  std::cout << "LEX-TREE VER 1.0\n\n";
+  return 0;
+}
